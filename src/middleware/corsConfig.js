bca83ae827b2d979d563b5fc2662aba88d8febf5
Mohammad Rahman <93023714+Rahman4758@users.noreply.github.com>
2025-03-17 01:39:38 +0530

    Update corsConfig.js

--- src/middleware/corsConfig.js
+++ src/middleware/corsConfig.js
@@ -1,7 +1,7 @@
 import cors from "cors";
 
 const corsOptions = {
-    origin: "https://lovable.dev/projects/6ed2da08-fc35-4423-9dae-0837560f32e0", // Change to frontend URL in production
+    origin: "*", // Change to frontend URL in production
     credentials: true,
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-Type", "Authorization"],
